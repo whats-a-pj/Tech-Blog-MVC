633e3c01775dd4db1c3dbbeaaff3faacf64284c7
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-11-27 13:34:53 -0700

    Sign up works, still pending dashboard

--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,7 @@ if (username && password) {
         headers: { 'Content-Type': 'application/json' },
     });
 if (response.ok) {
-        document.location.replace('/dashboard');
+        document.location.replace('/homepage');
     } else {
         alert(response.statusText);
         
@@ -54,26 +54,13 @@ if (username && password) {
     });
 
     if (response.ok) {
-        document.location.replace('/dashboard');
+        console.log(username)
+        document.location.replace('/homepage');
     } else {
         alert(response.statusText);
     }
     }
 };
 
-const logout = async () => {
-    const logout = await fetch('/api/users/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (logout.ok) {
-        document.location.replace('/');
-    } else {
-        alert(logout.statusText);
-    }
-};
-
 document.querySelector('#loginBtn').addEventListener('click', loginHandler);
 document.querySelector('#signUpForm').addEventListener('click', signupHandler);
-document.querySelector('#logout').addEventListener('click', logout);
